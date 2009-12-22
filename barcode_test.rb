07e3ab6051666fdc4ca2c4806007e090af8c5080
Petros Amiridis <petros@amiridis.net>
2009-12-23 01:09:47 +0200

    Added some support for calculating the check digit of EAN13

---
+++ barcode_test.rb
@@ -0,0 +1,11 @@
+require "barcode"
+require "test/unit"
+
+class BarcodeTest < Test::Unit::TestCase
+  def test_get_ean_13
+    assert_equal("5123456789123", Barcode.new("512345678912").get_ean_13)
+    assert_equal("5205324352841", Barcode.new("520532435284").get_ean_13)
+    assert_equal("1234567890128", Barcode.new("123456789012").get_ean_13)
+  end
+end
+
